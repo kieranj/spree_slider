94683b8bc74727156fb083baf9c10601b1e54f08
Ag <github@assembledgoods.com>
2012-10-25 20:40:42 -0500

    s3, move menu, disable all the overrides

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,5 +1,5 @@
 class Spree::Slide < ActiveRecord::Base
-  has_attached_file :image
+  has_attached_file :image, :storage => :s3, :s3_credentials => File.join(Rails.root, 'config', 's3.yml')
   scope :published, where(:published => true)
   attr_accessible :name, :body, :link_url, :published, :image, :position
 end
