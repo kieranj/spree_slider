209aedde9c78394684177d1ed2553a71e721ce76
Ag <github@assembledgoods.com>
2013-10-25 12:01:29 -0500

    revert

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,7 +1,7 @@
 class Spree::Slide < ActiveRecord::Base
   # include Spree::Core::S3Support
   # supports_s3 :image
-  has_attached_file :image#, :storage => :s3, :s3_credentials => File.join(Rails.root, 'config', 's3.yml')
+  has_attached_file :image, :storage => :s3, :s3_credentials => File.join(Rails.root, 'config', 's3.yml')
   scope :published, where(:published => true)
   attr_accessible :name, :body, :link_url, :published, :image, :position
 end
