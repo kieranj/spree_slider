15afac6a98c31b98055c1d75a3ed5fb07cec3518
Ag <github@assembledgoods.com>
2013-10-25 11:35:51 -0500

    remove s3, use built in

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,5 +1,7 @@
 class Spree::Slide < ActiveRecord::Base
-  has_attached_file :image, :storage => :s3, :s3_credentials => File.join(Rails.root, 'config', 's3.yml')
+  # include Spree::Core::S3Support
+  # supports_s3 :image
+  has_attached_file :image#, :storage => :s3, :s3_credentials => File.join(Rails.root, 'config', 's3.yml')
   scope :published, where(:published => true)
   attr_accessible :name, :body, :link_url, :published, :image, :position
 end
